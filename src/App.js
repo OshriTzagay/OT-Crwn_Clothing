289d7019b39927d63b9d05b78439ebda253cea74
OshriTzagay <oshritzagay@gmail.com>
2022-06-09 13:57:25 +0300

    adding redirect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Navigation from "./Components/routes/navigation/Nav";
 import Auth from "./Components/routes/Auth-Compo/Auth";
 import Shop from "../src/Components/routes/shop/shop";
 import CheckOut from "./Components/Check-Out-Compo/check-out";
+
+// https://62a1cccd148db15e5c8a2098--euphonious-cucurucho-c6f100.netlify.app/
+
+//!Deploy Link ^^^^^
+
 function App() {
   return (
     <Routes>
